de21f1865291359fdb613725165a63b3686701a2
Kent Hervey <kent.hervey@gmail.com>
2020-02-02 20:00:32 -0600

    Initial project setup commit

---
+++ src/main/java/com/autotrader/App.java
@@ -0,0 +1,13 @@
+package com.autotrader;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
