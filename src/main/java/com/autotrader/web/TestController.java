5f9e0f46410fcfe61c0273efffb18a5f4b4677fe
Kent Hervey <kent.hervey@gmail.com>
2020-02-08 16:55:55 -0600

    Set up initial spring boot application.  Also added test services endpoint.

---
+++ src/main/java/com/autotrader/web/TestController.java
@@ -0,0 +1,24 @@
+package com.autotrader.web;
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+
+    @GetMapping("/testServices")
+    public String testServices() {
+        int pass = 0;
+        int fail = 0;
+        String result = "Successfully  invoked endpoing. Running tests: \n";
+
+        //Add in calls to different test functionality and add to result string and counters
+
+        result += "Completed running tests. Passed: " + pass + " Failed: " + fail ;
+        return result;
+    }
+
+
+}
